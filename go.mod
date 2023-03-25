0effa07a5c4887b96d1783192dd3a12a285b2c27
AJackTi <dtrong97vn@gmail.com>
2023-03-25 20:08:46 +0700

    feat: update test func oauth

--- go.mod
+++ go.mod
@@ -4,4 +4,13 @@ go 1.20
 
 require github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
 
-require github.com/AJackTi/bookstore_utils-go v0.0.0-20230324063401-8948693471e3
+require (
+	github.com/AJackTi/bookstore_utils-go v0.0.0-20230324063401-8948693471e3
+	github.com/stretchr/testify v1.8.2
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ oauth/oauth_test.go
@@ -0,0 +1,49 @@
+package oauth
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("about to start main test")
+	os.Exit(m.Run())
+}
+
+func TestOauthConstants(t *testing.T) {
+	assert.EqualValues(t, "X-Public", headerXPublic)
+	assert.EqualValues(t, "X-Client-Id", headerXClientId)
+	assert.EqualValues(t, "X-Caller-Id", headerXCallerId)
+	assert.EqualValues(t, "access_token", paramAccessToken)
+}
+
+func TestIsPublicNilRequest(t *testing.T) {
+	assert.True(t, IsPublic(nil))
+}
+
+func TestIsPublicFalse(t *testing.T) {
+	request := http.Request{
+		Header: make(http.Header),
+	}
+
+	assert.False(t, IsPublic(&request))
+
+	request.Header.Add("X-Public", "true")
+	assert.True(t, IsPublic(&request))
+}
+
+func TestGetCallerIdNilRequest(t *testing.T) {
+
+}
+
+func TestGetCallerInvalidCallerFormat(t *testing.T) {
+
+}
+
+func TestGetCallerNoError(t *testing.T) {
+
+}
